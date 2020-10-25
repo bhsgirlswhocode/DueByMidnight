7ba9e7bcf8fb5a57d30f7ab62389aeef39fc2b63
bhsgirlswhocode <56425460+bhsgirlswhocode@users.noreply.github.com>
2020-10-24 18:25:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-https://repl.it/@MidnightCoders/htmlcssjsprogram#script.js
+var messages = document.getElementById("messages");
+var textbox = document.getElementById("textbox");
+var button = document.getElementById("button");
+
+button.addEventListener("click", function(){
+     var newMessage = document.createElement("li");
+     newMessage.innerHTML = textbox.value;
+     messages.appendChild(newMessage);
+     textbox.value = "";
+});
+
+
+function getName() {
+var nameField = document.getElementById('nameField').value;
+var result = document.getElementById('result');
+}
+
+function getEmail() {
+  var emailField = document.getElementById('emailField').value;
+  var result = document.getElementById('result');
+}
+
+
+/*
+function getprojname() {
+  var nameProj = document.getElementById('nameProj').value;
+  var result = document.getElementById('result');
+}
+ 
+const input = document.querySelector('input');
+const log = document.getElementById('log');
+
+input.addEventListener('change', updateValue);
+
+function updateValue(e) {
+  log.textContent = e.target.value;
+}
+*/
+
+$('button').click(function () {
+                    var str = $('#name').val()
+                    $('#template').html(str);
+                });
+
